3794f48e485651a26e7d562719a50f0be4c1408e
RyanX <ryan.guo99@gmail.com>
2018-02-20 00:57:57 -0500

    New language power-horse functions implemented

--- racket-graph/graphSyntax.rkt
+++ racket-graph/graphSyntax.rkt
@@ -43,39 +43,52 @@
   (displayln (graph-name G))
   (void (map (lambda (node)
                (displayln (list (node-name node)
-                                (node-neighbors node))))
+                                (node-edges node))))
              (graph-nodes G))))
 
 
-(struct graph [name nodes directed])
+(struct graph [name nodes directed] #:transparent)
 ;; A Graph is a (graph Symbol [List-of Node] Boolean)
-(struct node [name neighbors])
-;; A Node is a (node Symbol [List-of Symbol]))
+(struct node [name edges] #:transparent)
+;; A Node is a (node Symbol [List-of Edge]))
+(struct edge [to cost start duration] #:transparent)
+;; An edge is a (edge Symbol Number Number Number
+;; cost represents the monetary cost of this route/edge
+;; start represents the starting time (converted to a single number)
+;; duration represents how long the route takes
 
-;; A Path is a [List-of Symbol]
-;; Where symbols are names of nodes that connect
-;; (first path) to (last path)
+;; A Path is a [List [List-of Symbol] Number Number]
+;; Where symbols are names of nodes that connect the first symbol
+;; to the last in a graph.
+;; First number is the total monetary cost,
+;; second is time duration
 
 
 ;; -------------------------------------------------------------------
 ;; How to hide utility functions' scopes
 ;; Utility Functions for Graph
 ;; -------------------------------------------------------------------
-(define A (node 'A '(B E)))
-(define B (node 'B '(E F)))
-(define C (node 'C '(B D)))
-(define D (node 'D '()))
-(define E (node 'E '(C)))
-(define F (node 'F '(D G)))
-(define G (node 'G '()))
-(define sample-G (graph 'sample (list A B C D E F G) #t))
+#|
+
+A -> B 100 500 60
+A -> B 150 400 60
+B -> C 30 800 60
+B -> C 70 600 60
+
+
+|#
+(define A (node 'A (list (edge 'B 100 500 90)
+                         (edge 'B 150 400 60))))
+(define B (node 'B (list (edge 'C 30 800 120)
+                         (edge 'C 70 550 80))))
+(define C (node 'C '()))
+(define sample-G (graph 'sample (list A B C) #t))
 
 ;; Symbol Graph -> [Maybe Node]
 ;; Retrive the Node that has corresponding name from graph G
 ;; Return #f if the node does not exist in graph
 (module+ test
   (check-equal? (get-node 'B sample-G) B)
-  (check-equal? (get-node 'E sample-G) E)
   (check-equal? (get-node 'H sample-G) #f))
 
 (define (get-node name G)
@@ -88,42 +101,68 @@
 ;; Return a list of name of neighbors 
 ;; of the node with given name  in graph G
 (module+ test
-  (check-equal? (get-neighbors-name 'B sample-G)
-                '(E F))
-  (check-equal? (get-neighbors-name 'A sample-G)
-                '(B E))
-  (check-equal? (get-neighbors-name 'C sample-G)
-                '(B D)))
+  (check-equal? (get-edges 'B sample-G)
+                (list (edge 'C 30 800 60)
+                      (edge 'C 70 600 60)))
 
-(define (get-neighbors-name name G)
+  (check-equal? (get-edges 'A sample-G)
+                (list (edge 'B 100 500 60)
+                      (edge 'B 150 400 60)))
+
+  (check-equal? (get-edges 'C sample-G)
+                '()))
+
+(define (get-edges name G)
   (define the-node (get-node name G))
   (if the-node
-    (node-neighbors the-node)
+    (node-edges the-node)
     '()))
 
 
+;; Adds an edge to existing path
+;; Effectively returning an updated path including that edge
+(define (make-path eg path)
+  (list (cons (edge-to eg) (first path))
+        (+ (edge-cost eg) (second path))
+        (+ (edge-duration eg) (third path))))
 
-; Symbol Symbol Graph -> [List-of Path]
+;; Similar to make-path
+;; Adds an edge to update each path in given list of path lop
+(define (add-to-paths eg lop)
+  (map (lambda (a-path)
+         (cons eg a-path))
+       lop))
+
+; Symbol Symbol Graph Number -> [List-of Path]
 ; finds a all paths from name of node ori to name of node des in graph G
+; current time set to be curr
 ; If no such path exist, the list contains names of both origin and destination,
 ; and name of all the intermediate nodes.
-(define (find-all-path ori des G)
+(define (find-all-path ori des G curr)
   (local (;; Accumulator seen:
           ;; A list of symbols that represents the name of nodes
           ;; which have been visited
           ;; ASSUME:
           ;; ori is not in seen
-          (define (find-all-path-acc ori des G seen)
+          (define (find-all-path-acc ori des G seen curr)
             (cond
-              [(symbol=? ori des) `((,des))]
-              [else (define neighbors (get-neighbors-name ori G))
+              [(symbol=? ori des) `(())]
+              [else (define neighbors (get-edges ori G))
+
                     ;; Filter out neighbors that have been seen
-                    (define valid-neighbors (filter (lambda (name)
-                                                      (not (member name seen)))
+                    ;; [List-of Edges]
+                    (define valid-neighbors (filter (lambda (an-edge)
+                                                      (and (not (member (edge-to an-edge) seen))
+                                                           (< curr (edge-start an-edge))))
                                                     neighbors))
+
                     ;; [List-of [List-of Path]]
-                    (define results (map (lambda (name) 
-                                           (find-all-path-acc name des G (cons name seen))) 
+                    (define results (map (lambda (an-edge) 
+                                           (add-to-paths an-edge
+                                                         (find-all-path-acc (edge-to an-edge) des G 
+                                                                            (cons (edge-to an-edge) seen)
+                                                                            (+ (edge-duration an-edge) 
+                                                                               (max curr (edge-start an-edge))))))
                                          valid-neighbors))
                     ;; [List-of Path]
                     ;; Paths from all of ori's valid neighbors to
@@ -134,32 +173,13 @@
                                        lop-sofar))
                              '()
                              results))
-                    ;; [List-of Path]
-                    ;; All Possible Paths from ori to des
-                    (define final-results
-                      (map (lambda (path) (cons ori path)) flattened-results))
 
-                    final-results])))
-    (find-all-path-acc ori des G '())))
+                    flattened-results])))
+    (find-all-path-acc ori des G '() curr)))
 
-(module+ test
-  (check-equal? (find-all-path 'A 'G sample-G)
-                '((A B F G)
-                  (A E C B F G)))
+(find-all-path 'A 'C sample-G 350)
 
-  (check-equal? (find-all-path 'G 'A sample-G)
-                '())
-
-  (check-equal? (find-all-path 'D 'C sample-G)
-                '())
-
-  (check-equal? (find-all-path 'A 'D sample-G)
-                '((A B E C D)
-                  (A B F D)
-                  (A E C B F D)
-                  (A E C D))))
-
-
+(module+ test)
 
 
 
