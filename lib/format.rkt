993008807f11ded8c11ff0c20376b4328cb49884
Samedh <gupta.sam@husky.neu.edu>
2018-03-12 15:45:26 -0400

    improved the format functions

--- lib/format.rkt
+++ lib/format.rkt
@@ -25,38 +25,49 @@
 
 
 ;; Processed-Path -> String
-;; ...
+;; gets all the information for this processed path as a string
 (define (pp->string pp)
+  ;; the list of edges for this path
   (define path (path-loe (processed-path-path pp)))
-  (define list-ori (map (λ (p) (symbol->string (edge-from p))) path))
-  (define list-des (map (λ (p) (symbol->string (edge-to p))) path))
-  (define list-start (map edge-start path))
-  (define list-end (map edge-end path))
-  (define list-cost (map edge-cost path))
-  (define col1 (string-length (argmax string-length (cons "From" list-ori))))
-  (define col2 (string-length (argmax string-length (cons "To" list-des))))
-  (define (combine ori des start end cost)
+  ;; the length of the first column
+  (define col1 (string-length
+                (argmax string-length
+                        (cons "From"
+                              (map (λ (p) (symbol->string (edge-from p))) path)))))
+  ;; the length of the second column
+  (define col2 (string-length
+                (argmax string-length
+                        (cons "To"
+                              (map (λ (p) (symbol->string (edge-to p))) path)))))
+  ;; edge -> string
+  ;; makes a string for the edge
+  (define (combine e)
     (string-append
-     (~a ori #:min-width col1)
+     (~a (edge-from e) #:min-width col1)
      "   "
-     (~a des #:min-width col2)
+     (~a (edge-to e) #:min-width col2)
      "   "
-     (seconds->date-str start)
+     (seconds->date-str (edge-start e))
      "  "
-     (seconds->date-str end)
+     (seconds->date-str (edge-end e))
      "  "
-     (number->string cost)
+     (number->string (edge-cost e))
      "\n"))
+
+  ;; the table of iformation for this path
   (define main (foldr string-append 
                       "" 
-                      (map combine list-ori list-des list-start list-end list-cost)))
+                      (map combine path)))
+  ;; the header column for the table
+  (define header (string-append (~a "From" #:min-width col1) "   " (~a "To" #:min-width col2)
+                  "   Departure Time      Arrival Time        Cost\n"))
+  ;; finally append all the data
   (string-append
    sep*
    "Number of intermediate stops : " (number->string (processed-path-stop-count pp)) "\n"
    "Total Price : $" (number->string (processed-path-cost pp)) "\n"
    "Total Duration : " (real->decimal-string (/ (processed-path-duration pp) 3600)) " hrs\n\n"
-   (string-append (~a "From" #:min-width col1) "   " (~a "To" #:min-width col2)
-                  "   Departure Time      Arrival Time        Cost\n")
+   header
    main))
 
 (define sep*
@@ -76,42 +87,3 @@
   221400
   133200
   1)))
-
-
-
-#|
-;; A -> B t1 t2 $1000
-(define los '("Boston" "JFK" "Shanghai"))
-(define lod '("JFK" "Shanghai" "Beijing"))
-
-(define n1 (string-length (argmax string-length (cons "Origin" los))))
-(define n2 (string-length (argmax string-length (cons "Destination" lod))))
-
-
-
-(define (out ori n1 des n2 others)
-  (printf 
-    (string-append
-      (~a ori #:min-width n1)
-      "        "
-      (~a des #:min-width n2)
-      others)))
-
-(define sep*
-  "****************************************************************************************\n")
-(define sep=
-  "========================================================================================\n")
-
-(printf sep=)
-(printf "Plan A\n")
-(printf "From Boston to Shanghai\n\n")
-(printf sep*)
-(printf "Number of Intermediate Stops: 2\n")
-(printf "Total Price:     $4602\n")
-(printf "Total Duration   46 hours\n\n")
-(out "Origin" n1 "Destination" n2 "   Departure Time    Arrival Time      Cost \n")
-(out "Boston" n1 "JFK" n2 "   16:14 02/02/2018  19:00 02/03/2018  $1534\n")
-(out "JFK" n1 "Shanghai" n2  "   22:00 02/02/2018  02:00 02/04/2018  $1534\n")
-(out "Shanghai" n1 "Beijing" n2  "   20:00 02/04/2018  02:00 02/05/2018  $1534\n")
-(printf sep*)
-(printf sep=)|#
